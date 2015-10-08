e162556280c31b0150a52838552debc1b98aa917
Athinodoros <athinodoros.sgouromallis@hotmail.com>
2015-10-08 14:03:09 +0200

    company table

--- web/scripts/company.js
+++ web/scripts/company.js
@@ -5,3 +5,36 @@
  */
 
 
+$(document).ready(function (){
+    $url ='/CA2/api/company';
+    $.ajax({
+            type : 'GET',
+            url : $url +"/all",
+            datatype : "json",
+            success: function (data, textStatus, jqXHR) {
+            console.log(data.length);
+          
+                for (var i = 0; i < data.length; i++) {
+                    var cvr = "<td>"+data[i].cvr+"</td>";
+                    var numOfEmployees = "<td>"+data[i].numOfEmployees+"</td>";
+                    var id = "<td>"+data[i].id+"</td>";
+                    var email = "<td>"+data[i].email+"</td>";
+                    var phonelist;
+                    for (var j = 0; i <data[i].phones; j++) {
+                        phonelist = data[i].phones[j]+ "<br/>"
+                    }
+                    var phones = "<td>"+phonelist+"</td>";
+                    console.log("<tr>"+id+cvr+numOfEmployees+email+phones+"</tr>");
+                    $("#table").append("<tr>"+id+cvr+numOfEmployees+email+phones+"</tr>");
+                }
+               
+                
+           
+        }
+            
+    });
+    
+    
+    
+    
+});
