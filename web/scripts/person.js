d5b6bdf348867d09ddf4396a133165ab6db6130e
fandul <rihards.pacevics@gmail.com>
2015-10-09 14:34:48 +0200

    person

--- web/scripts/person.js
+++ web/scripts/person.js
@@ -20,11 +20,15 @@ $(document).ready(function(){
                     var firstName = "<td>"+data[i].firstName+"</td>";
                     var lastName = "<td>"+data[i].lastName+"</td>";
                     var email = "<td>"+data[i].email+"</td>";
+                    var street = "<td>"+data[i].streetName+"</td>";
+                    var city = "<td>"+data[i].city+"</td>";
                     var phonelist;
                     for (var j = 0; i <data[i].phones.lenght; j++) {
                         phonelist = data[i].phones[j].phoneNumber+ "<br/>";
                         //console.log(data[i].phones);
                     }
+                    var $edit = "<td><a href='/CA2/edit/edit.html' class='" + data[i].id + "' data-id='" + data[i].id + "' >edit</a>  ";
+                    var $delete = " / <a href='#' class='" + data[i].id + "' >delete</a> </td> ";
                      console.log(data[i].phones);
                     var hobies;
                     for (var j = 0; i <data[i].hobby; j++) {
@@ -32,19 +36,39 @@ $(document).ready(function(){
                     }
                     var phones = "<td>"+phonelist+"</td>";
                     //console.log("<tr>"+id+firstName+lastName+email+phones+"</tr>");
-                    $("#table").append("<tr>"+id+firstName+lastName+phones+email+"</tr>");
+                    $("#table").append("<tr>"+id+firstName+lastName+phones+email+street+city+$edit + $delete+"</tr>");
                 }
             }
         
     });
     };
     load();
-    
+//    // checks if there is input in fields
+//    $("#myForm").validate({
+//                rules: {
+//                    fname: {
+//                        required: true
+//                    },
+//                    lname:{
+//                        required: true,
+//                    },
+//                    phone: {
+//                        required: true
+//                    },
+//                    email: {
+//                        required: true,
+//                    },
+//                },
+//                submitHandler: function(form) {
+//                     form.submit();
+//                 }
+//      });
+      
     $("#submit").click(function() {
         var $type = "POST";
         var data = {firstName: $("#fname").val(), lastName: $("#lname").val(), 
              //phones:{phoneNumber:$("#phone").val()},
-             email: $("#email").val()};
+             email: $("#email").val(), streetName: $("#street").val(), city: $("#city").val() };
         var $url = "/CA2/api/person";
         $.ajax({
             type: $type,
@@ -57,6 +81,33 @@ $(document).ready(function(){
                 load();
             }
         });
+    });
+    
+        $(document).on("click", "a", function () {
+
+        var act = "company";
+        var myA = this;
+        if (this.innerText === "edit") {
+            localStorage.setItem("id",$(myA).attr('class') );
+            var val = localStorage.getItem("xxx");
+            console.log("sadsgadfijcakvlmsdvamwdv     " + val);
+            document.localStorage.
+                    $(window).load($(myA).attr("href"));
+        } else if (this.innerText === "delete") {
+
+            $.ajax({
+                type: 'DELETE',
+                url: '/CA2/api/person/' + $(myA).attr("class"),
+                success: function (data, textStatus, jqXHR) {
+                    location.reload();
+                    load();
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert("errrrrrooor");
+                }
+            });
+
+        }
     });
 });
 
